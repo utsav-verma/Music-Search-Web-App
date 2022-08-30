b0953626c26b484152d897d0ef6e46b6d01ae519
utsav-verma <67074628+utsav-verma@users.noreply.github.com>
2022-08-30 20:22:31 +0530

    Add files via upload

---
+++ music_search_bar/script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("src-result").onclick - function()
+{
+    var srch = document.getElementById("input-get").value;
+    DocumentFragment.getElementById("input-get").value="amazon.in"
+    console.log(srch);
+}
