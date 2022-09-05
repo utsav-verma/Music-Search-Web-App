c768b57dbb8a649c5736e9cc758536a721c43e13
utsav-verma <67074628+utsav-verma@users.noreply.github.com>
2022-09-05 18:28:12 +0530

    Add files via upload

--- music_search_bar/script.js
+++ music_search_bar/script.js
@@ -1,7 +1,9 @@
 
-document.getElementById("src-result").onclick - function()
-{
-    var srch = document.getElementById("input-get").value;
-    DocumentFragment.getElementById("input-get").value="amazon.in"
-    console.log(srch);
-}
+function demo(){
+    var src = document.getElementById('input-get').value;
+    src +=" "+" site:amazon.in OR site:flipkart.com";
+    var url = new URL("http://www.google.com/search?q=");
+    url+=src;
+    window.location.href =url;
+    
+    }
